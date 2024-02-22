d096caeddbd3ea49956609ba20bcc9f5e991555c
Karengrabarz <karengrabarz@gmail.com>
2024-02-22 10:22:32 -0300

    fix:style cart

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -3,41 +3,40 @@ import { MdDelete } from "react-icons/md";
 import style from "./style.module.scss";
 
 export const CartItemCard = ({removeItemCart, removeCount, product,addItemCart }) => {
-  
-  const total = product.price * product.quantity
-  
-  
+   
   return (
     <li className={style.flexBox}>
       <div>
         <img src={product.img} alt={product.name} />
       </div>
-      <div>
-        <h3 className="title grey600">{product.name}</h3>
-        <div className={style.qty}>
-          <button onClick={()=>{
-            removeItemCart(product.id)
-            removeCount()
-          }}
-          >-</button>
-          <span>{product.quantity}</span>
-          <button  onClick={() => {
-              addItemCart(product);
-            }}>+</button>
+      <div className={style.cart} >
+        <h3 className="title-sm grey600">{product.name}</h3>
+        <div className={style.priceCart} >
+          <div className={style.qty}>
+            <button onClick={()=>{
+              removeItemCart(product.id)
+              removeCount()
+            }}
+            >-</button>
+            <span>{product.quantity}</span>
+            <button  onClick={() => {
+                addItemCart(product);
+              }}>+</button>
+          </div>
+          <div>
+            R$ {product.price * product.quantity}
+          </div>
+          <button
+            aria-label="delete"
+            title="Remover item"
+            onClick={() => {
+              removeCount();
+              removeItemCart(product.id);
+            }}
+          >
+            <MdDelete size={21} />
+          </button>
         </div>
-        <div>
-          R$ {total}
-        </div>
-        <button
-          aria-label="delete"
-          title="Remover item"
-          onClick={() => {
-            removeCount();
-            removeItemCart(product.id);
-          }}
-        >
-          <MdDelete size={21} />
-        </button>
       </div>
     </li>
   );
